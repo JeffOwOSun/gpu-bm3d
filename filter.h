2e12ab1fd092b03648e7fa6cda543cbcaca52473
TianxiongWang <nickwang57@gmail.com>
2017-11-22 16:10:58 -0500

    tweak constant struct

--- filter.h
+++ filter.h
@@ -3,6 +3,27 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+/*
+ * Read-only variables for all cuda kernels. These variables
+ * will be stored in the "constant" memory on GPU for fast read.
+ */
+struct GlobalConstants {
+
+    unsigned int searching_window_size;
+    unsigned int patch_size;
+    unsigned int max_group_size;
+    unsigned int distance_threshold_1;
+    unsigned int distance_threshold_2;
+    unsigned int stripe;
+    float sigma;
+    float lambda_3d;
+    float beta;
+
+    int image_channels;
+    int image_width;
+    int image_height;
+    float* image_data;
+};
 
 __constant__ GlobalConstants cu_const_params;
 
